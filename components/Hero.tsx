a3ac6e5c9a75b4be306040d9e05dd84746452e33
AghaJeevaySoft <rabialatif313@gmail.com>
2024-07-30 20:20:17 +0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Rabia, a UI/UX Designer and Website Developer based in
+            Hi! I&apos;m Rabia, a UI/UX Designer and Web Developer based in
             Pakistan.
           </p>
 
